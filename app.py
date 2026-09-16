5627599ee99fdbede7b0961e0fbe44d0c981fc30
linzoldyck8-ship-it <linzoldyck8@gmail.com>
2026-09-16 20:24:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def conectar_gsheets():
         client = gspread.authorize(creds)
         sheet = client.open_by_key(SHEET_ID).sheet1
         return sheet
-   except Exception as e:
+ except Exception as e:
         st.error(f"Error real de conexión: {e}")
         return None
 
