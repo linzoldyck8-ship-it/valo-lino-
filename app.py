b853837e528844d0d29c152e910969bf6fe1ee37
linzoldyck8-ship-it <linzoldyck8@gmail.com>
2026-09-16 18:16:46 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ from streamlit_autorefresh import st_autorefresh
 # Configuración de la página web
 st.set_page_config(
     page_title="Scarlet Valorant - Reclutamiento",
-    page_icon="SCARLET.png",  # Asegúrate de subir también este ícono a GitHub
+    page_icon="logo.png",  # Asegúrate de subir también este ícono a GitHub
     layout="wide"
 )
 
 # --- ESTILOS CSS CON IMAGEN DE FONDO VÍA URL WEB ---
 st.markdown("""
     <style>
-    @import url('https://www.google.com/imgres?q=imagen%20en%20CARMESI%201280x720&imgurl=https%3A%2F%2Fp4.wallpaperbetter.com%2Fwallpaper%2F647%2F12%2F871%2Fcrimson-red-wallpaper-preview.jpg&imgrefurl=https%3A%2F%2Fwww.wallpaperbetter.com%2Fes%2Fsearch%3Fq%3Drojo%2Bcarmes%25C3%25AD&docid=2URoEagGMnMqIM&tbnid=jC00c56onfu6DM&vet=12ahUKEwjH27GLgvSWAxVBEbkGHQVaIIoQnPAOegUI_gEQAA..i&w=728&h=455&hcb=2&itg=1&ved=2ahUKEwjH27GLgvSWAxVBEbkGHQVaIIoQnPAOegUI_gEQAA');
+    @import url('https://fonts.googleapis.com/css2?family=Agdasima:wght@400;700&display=swap');
 
     html, body, [class*="css"] {
         font-family: 'Agdasima', sans-serif !important;
@@ -21,7 +21,7 @@ st.markdown("""
 
     /* Fondo con imagen web y capa oscura neón */
     .stApp {
-        background-image: linear-gradient(rgba(11, 13, 18, 0.88), rgba(11, 13, 18, 0.92)), url("https://www.reddit.com/r/wallpapers/comments/261uih/simple_black_and_red_block_wallpaper_1920x1080/?tl=es-419"); /* <--- Cambia este enlace por el de tu imagen de fondo */
+        background-image: linear-gradient(rgba(11, 13, 18, 0.88), rgba(11, 13, 18, 0.92)), url("https://images.unsplash.com/photo-1542751371-adc38448a05e?q=80&w=1920"); /* <--- Cambia este enlace por el de tu imagen de fondo */
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
