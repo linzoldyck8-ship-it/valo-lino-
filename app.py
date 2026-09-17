917c5d65ec2796edaa579c8d41847311930156fb
linzoldyck8-ship-it <linzoldyck8@gmail.com>
2026-09-16 22:31:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from google.oauth2.service_account import Credentials
 # Configuración de la página web
 st.set_page_config(
     page_title="Scarlet Valorant - Reclutamiento",
-    page_icon="SCARLET.png"  # <-- Coloca aquí el nombre/ruta de tu imagen en GitHub
-    layout="wide"
+    page_icon="SCARLET.png",  # <-- Coloca aquí el nombre/ruta de tu imagen en GitHub
+
 )
 
 # --- ESTILOS CSS AVANZADOS ---
